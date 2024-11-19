b2c8416f94b9f104ca757fcfb91a7b6775b70789
Baker Sharp <bsharp@milkenschool.org>
2024-11-19 09:10:15 -0800

    test submit 2

--- python-test/hello-world.py
+++
@@ -1,7 +0,0 @@
-print("Hello World!")
-
-# Use this file to test if python is working!
-# cd into this folder then run it with
-# `python hello-world.py` or `python3 hello-world.py`
-
-print("See this in terminal? Looks like you got Python working, nice!")
